7ecdf6ffc8980ad38a9c433c62064a1e20c28520
Nobin Mathew <nobin.mathew@gmail.com>
2012-04-06 20:53:32 +0530

    Adding some editor changes to gae branch

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import models.Post;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
+import java.util.*;
 
 @With(Secure.class)
 public class Admin extends Controller {
@@ -15,8 +17,19 @@ public class Admin extends Controller {
 		}
 	}	
 	
-	public static void index() {		
+	public static void index() {
+		String user = Security.connected();
+		User loginuser = User.all().filter("email", user).get();
+		List <Post> posts = Post.all().filter("author", loginuser).fetch();		
+		render(posts);
+	}
+	
+	public static void form() {
 		render();
+	}
+	
+	public static void save() {
+		
 	}
 
 }
