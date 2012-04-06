72dc13b1c056a1ed51b4235f06a10669ed831399
Nobin Mathew <nobin.mathew@gmail.com>
2012-04-06 17:10:35 +0530

    Completed JPA version of Blog engine

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -5,6 +5,8 @@ import play.mvc.*;
 
 import java.util.*;
 
+import models.Post;
+import models.Tag;
 import models.User;
 
 @With(Secure.class)
@@ -18,7 +20,43 @@ public class Admin extends Controller {
 	}
 	
 	public static void index() {
+		String user = Security.connected();
+		List<Post> posts = Post.find("author.email", user).fetch();		
+		render(posts);
+	}
+	
+	public static void form(Long id) {
+		if(id != null) {		
+			Post post = Post.findById(id);
+			render(post);
+		}
 		render();
 	}
-
+	
+	public static void save(Long id, String title, String content, String tags) {
+		Post post;
+		if(id == null) {
+			User author = User.find("byEmail", Security.connected()).first();
+			post = new Post(title, author, content);
+		}
+		else {
+			post = Post.findById(id) ;
+			post.title = title;
+			post.content = content;
+			post.tags.clear();
+		}			
+		
+		for(String tag : tags.split("\\s+")) {
+			if(tag.trim().length() > 0)
+				post.tags.add(Tag.findOrCreateByName(tag));
+		}
+		validation.valid(post);
+		
+		if(validation.hasErrors()) {
+			render("@form", post);
+		}
+		
+		post.save();
+		index();
+	}
 }
